c1f6b9d4fd2f4c5698986ad1506e7bce1bc0e599
Evan Dana <evan.dana@gmail.com>
2017-12-18 12:41:51 -0500

    start all now works

--- 1-generate/install.sh
+++
@@ -1,6 +0,0 @@
-pwd
-LOCATION=$1
-echo "loc:${LOCATION}"
-pwd
-npm install
-osascript -e 'tell application "Terminal" to close (every window whose name contains "1-generate")' &
--- runall.sh
+++ runall.sh
@@ -1,5 +1,5 @@
 # opens a new window for each run
 # leaves window open
-./scripts/opennewterminal.sh 1-generate "npm start"
-./scripts/opennewterminal.sh 2-process "npm start"
-./scripts/opennewterminal.sh 3-visualize "npm start"
+./scripts/opennewterminal.sh 1-generate "node ./index.js" #`npm start` is not working
+./scripts/opennewterminal.sh 2-process "node ./index.js" #`npm start` is not working
+./scripts/opennewterminal.sh 3-visualize "node ./index.js" #`npm start` is not working
